5864daa892476f1e9e6e5a2a0b3e1a05f40947a0
maxramirez84 <maxramirez84@gmail.com>
2021-05-18 14:34:44 +0200

    As the profile project is also included in the tokens list, jump it to avoid crashing due to different data dictionary.

--- icaria_progress.R
+++ icaria_progress.R
@@ -1,6 +1,7 @@
 library(redcapAPI)
 
 ReadData <- function(api.url, api.token) {
+  #browser()
   rcon <- redcapConnection(api.url, api.token)
-  data <- exportRecords(rcon)
+  data <- exportRecords(rcon, factors = F, labels = F)
 }
