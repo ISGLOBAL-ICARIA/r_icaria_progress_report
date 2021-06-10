dffa8ffe481d478680932cc7ff4f9d19ab1f290d
maxramirez84 <maxramirez84@gmail.com>
2021-06-10 10:20:14 +0200

    New function to make a summary of the Screening Log data by health facility.

--- icaria_progress.R
+++ icaria_progress.R
@@ -5,3 +5,31 @@ ReadData <- function(api.url, api.token) {
   rcon <- redcapConnection(api.url, api.token)
   data <- exportRecords(rcon, factors = F, labels = F)
 }
+
+SummarizeData <- function(hf.list, profile, data) {
+  # Collapse HF IDs in the hf column no matter in which district the HF is
+  profile$hf <- rowSums(
+    x     = profile[, c("hf_bombali", "hf_tonkolili", "hf_port_loko")], 
+    na.rm = T
+  )
+  
+  # Trial profile variables to be summarized
+  vars <- c('n_penta1', 'n_approached', 'n_underweight', 'n_over_age', 
+            'n_consent')
+  
+  # Summarized all trial profiles varibles by health facility
+  summary <- data.frame(hf.list)
+  for (var in vars) {
+    col <- c()
+    for (hf in hf.list) {
+      col[hf] <- sum(profile[which(profile$hf == hf), var], na.rm = T)
+    }
+    summary[var] <- col
+  }
+  
+  # Compute the number of refusals based on the captured variables
+  summary$n_refusal <- summary$n_approached - summary$n_underweight - 
+    summary$n_over_age - summary$n_consent
+  
+  return(summary)
+}
